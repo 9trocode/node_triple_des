ed0f7b3aadba1f5d980df71ddf4154641cda8583
nitrocode <alexidowu25@gmail.com>
2020-03-02 10:41:38 +0100

    Still working on decrypt functionality

---
+++ test/sdk.test.js
@@ -0,0 +1,18 @@
+const assert = require('assert');
+const { expect }= require('chai');
+const Encryption = require('../index');
+
+describe('Simple Storage Test', () => {
+
+    //First stage Unit Test
+    it('should run encrypt', () => {
+        const data =  Encryption.encrypt('834y9yuhjsdjfdsjfbk++', 'testing');
+          console.log(data);
+    });
+
+    it('should run decrypt', () => {
+        const data =  Encryption.decrypt('834y9yuhjsdjfdsjfbk++', 'testing');
+          console.log(data);
+    });
+
+});
